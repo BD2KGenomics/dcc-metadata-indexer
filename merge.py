4ec8336ec849c78fd8e4305164fad41299c53e63
Emily Kobayashi <emily@CBSEUSER-MacBook-Pro.local>
2016-05-24 16:36:07 -0700

    Can query using esquery.py and jqueryflag. Updated merge.py to conform the the metadata_model_no_uri.json schema.

--- merge.py
+++ merge.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from jsonmerge import merge
+from jsonspec.validators import load    #for validation
 import json
 
 files = ['sample_individual_metadata_bundle_jsons/','1a_donor_biospecimen.json','1b_donor_biospecimen.json','2a_fastq_upload.json','2b_fastq_upload.json','3a_alignment.json','3b_alignment.json','4_variant_calling.json']
@@ -18,10 +19,10 @@ def openFiles(files):
       try: 
          with open(files[0]+files[i+1]) as data_file:
             data.append(json.load(data_file))
-         flags.append(True)
+         flags.append("true")
       except FileNotFoundError:
          print ('File not found: '+files[i+1])
-         flags.append(False)
+         flags.append("false")
          data.append(0)
          
 def assignBranch(data, flags):
@@ -30,7 +31,7 @@ def assignBranch(data, flags):
    i=0 
    k=0
    for j in range(2,6):
-      if (flags[j] == True):
+      if (flags[j] == "true"):
          workflows={}
          for uuid in data[j]['parent_uuids']:
             #print ("UUIDs: "+uuid)
@@ -48,7 +49,7 @@ def assignBranch(data, flags):
          k=1
 
 def assignVariant(data):
-   if (flags[6] == True):
+   if (flags[6] == "true"):
       workflows={}
       for uuid in data[6]['parent_uuids']:
          #print ("UUIDs: "+uuid)
@@ -59,18 +60,38 @@ def assignVariant(data):
 
 def dumpResult(result):
    with open('merge.json', 'w') as outfile:
+      #newidtype = dict(_id=1, _type='meta') #could use this for elasticsearch bulk queries
+      #newindex = dict(index = newidtype)
+      #json.dump(newindex, outfile)
+      #outfile.write('\n')
       json.dump(result, outfile)
       
 def createFlags(flags):
    flagsWithStr = dict(zip(['donor1_exists', 'donor2_exists', 'fastqNormal_exists', 'fastqTumor_exists', 'alignmentNormal_exists', 'alignmentTumor_exists', 'variantCalling_exists'], flags))
    result['flags'] = flagsWithStr
-      
+
+def validateResult():
+   # data will validate against this schema
+   with open('metadata_model_no_uri.json') as data_file:    
+      schema = json.load(data_file)
+
+   #loading into jsonspec
+   validator = load(schema)
+
+   # validate result against schema
+   validator.validate(result)
+
 openFiles(files)
 #Assuming that the donor documents will always exist
 result = merge(data[0], data[1])   
 assignBranch(data, flags)
 assignVariant(data)
 createFlags(flags)
+validateResult()
 dumpResult(result)
-
 #pprint(result)
+
+
+
+
+
