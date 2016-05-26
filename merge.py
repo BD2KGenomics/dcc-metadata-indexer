d8b087f8afddc1936f494455d76e5c79de19389a
Emily Kobayashi <emily@CBSEUSER-MacBook-Pro.local>
2016-05-26 10:58:13 -0700

    Working on making merge.py and esquery.py work together

--- merge.py
+++ merge.py
@@ -2,19 +2,12 @@ from pprint import pprint
 from jsonmerge import merge
 from jsonspec.validators import load    #for validation
 import json
-
-files = ['sample_individual_metadata_bundle_jsons/','1a_donor_biospecimen.json','1b_donor_biospecimen.json','2a_fastq_upload.json','2b_fastq_upload.json','3a_alignment.json','3b_alignment.json','4_variant_calling.json']
-data = []
-flags = []
-specimen_type = ['normal_specimen','tumor_specimen'] 
-type = ['samples','sample_uuid','sequence_upload','alignment']
-result = {}
-flagsWithStr = {}
+import random
 
 #Note: the files must be in this particular order:
 #folderName, donor, donor, fastqNormal, fastqTumor, alignmentNormal, alignmentTumor, variantCalling
 
-def openFiles(files):
+def openFiles(files, data, flags):
    for i in range(len(files)-1):
       try: 
          with open(files[0]+files[i+1]) as data_file:
@@ -25,9 +18,11 @@ def openFiles(files):
          flags.append("false")
          data.append(0)
          
-def assignBranch(data, flags):
+def assignBranch(data, flags, result):
    # finds the uuid in 2a, 2b, 3a, 3b and then adds data to the correct branch
    # j controls the type (normal or tumor). k and i place the data in the correct place
+   specimen_type = ['normal_specimen','tumor_specimen'] 
+   type = ['samples','sample_uuid','sequence_upload','alignment']
    i=0 
    k=0
    for j in range(2,6):
@@ -48,7 +43,7 @@ def assignBranch(data, flags):
       if (i%2==0):
          k=1
 
-def assignVariant(data):
+def assignVariant(data, flags, result):
    if (flags[6] == "true"):
       workflows={}
       for uuid in data[6]['parent_uuids']:
@@ -57,20 +52,20 @@ def assignVariant(data):
       donor_uuid = result['donor_uuid']
       result['somatic_variant_calling'] = workflows[donor_uuid]
 
-
 def dumpResult(result):
-   with open('merge.json', 'w') as outfile:
+   with open('merge.json', 'a') as outfile:
       #newidtype = dict(_id=1, _type='meta') #could use this for elasticsearch bulk queries
       #newindex = dict(index = newidtype)
       #json.dump(newindex, outfile)
       #outfile.write('\n')
       json.dump(result, outfile)
+      outfile.write('\n')
       
-def createFlags(flags):
+def createFlags(flags, result):
    flagsWithStr = dict(zip(['donor1_exists', 'donor2_exists', 'fastqNormal_exists', 'fastqTumor_exists', 'alignmentNormal_exists', 'alignmentTumor_exists', 'variantCalling_exists'], flags))
    result['flags'] = flagsWithStr
 
-def validateResult():
+def validateResult(result):
    # data will validate against this schema
    with open('metadata_model_no_uri.json') as data_file:    
       schema = json.load(data_file)
@@ -81,17 +76,36 @@ def validateResult():
    # validate result against schema
    validator.validate(result)
 
-openFiles(files)
-#Assuming that the donor documents will always exist
-result = merge(data[0], data[1])   
-assignBranch(data, flags)
-assignVariant(data)
-createFlags(flags)
-validateResult()
-dumpResult(result)
-#pprint(result)
+def run(files):
+   data = []
+   flags = []
+   result = {}
+   flagsWithStr = {}
+   openFiles(files, data, flags)
+   result = merge(data[0], data[1])  
+   assignBranch(data, flags, result)
+   assignVariant(data, flags, result)
+   validateResult(result)
+   createFlags(flags, result)
+   dumpResult(result)
+   #pprint(result)
 
+def fileRandom(files):
+   file = ['sample_individual_metadata_bundle_jsons/','1a_donor_biospecimen.json','1b_donor_biospecimen.json','2a_fastq_upload.json','2b_fastq_upload.json','3a_alignment.json','3b_alignment.json','4_variant_calling.json']
+   files.append(file[0])
+   files.append(file[1])
+   files.append(file[2])
+   for f in range(3,8):
+      x = random.randint(1, 10)
+      if (x>=5):
+         files.append(file[f])
+      else:
+         files.append("no"+file[f])
 
-
+for a in range(10):
+   print (a)
+   files = []
+   fileRandom(files)
+   run(files)
 
 
