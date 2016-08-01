48a95c1d2bbb6893e8b5fdf870de02e795a656b6
Brian O'Connor <briandoconnor@gmail.com>
2016-07-31 21:54:10 -0700

    working on Luigi piece

--- luigi_task_executor/AlignmentQCTask.py
+++ luigi_task_executor/AlignmentQCTask.py
@@ -1,15 +1,19 @@
 import luigi
 import json
 import time
+import re
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
 # TODO:
 # * need a tool that takes bundle ID and file path and converts to file_id for downloading by Dockstore CLI
 # * I can fire off a Dockstore CLI to write the data back but who makes a metadata.json?  Should I add this to Dockstore CLI?  Add it here?
-#   Probably, for now I can do everything in this task (maybe distinct tasks?) and then, over time, push this out to Dockstore CLI.
+#   Probably, for now I can do everything in this task (maybe distinct tasks for download, upload, tool run)
+#   and then, over time, push this out to Dockstore CLI.
 #   So, effectively, I use Luigi really as a workflow but, ultimately, this will need to move out to worker VMs via Consonance and be done via
 #   Dockstore CLI (or Toil) in order to gain flexibility.
+# RUNNING
+# $> rm /tmp/foo-*; PYTHONPATH='' luigi --module AlignmentQCTask AlignmentQCCoordinator --es-index-host localhost --es-index-port 9200
 
 class AlignmentQCTaskWorker(luigi.Task):
     param = luigi.Parameter(default=42)
@@ -29,15 +33,23 @@ class AlignmentQCCoordinator(luigi.Task):
 
     def requires(self):
         es = Elasticsearch([{'host': self.es_index_host, 'port': self.es_index_port}])
-# TODO: customize query to find only suitible donors
-        res = es.search(index="analysis_index", body={"query": {"match_all": {}}})
+        # see jqueryflag_alignment_qc
+        # curl -XPOST http://localhost:9200/analysis_index/_search?pretty -d @jqueryflag_alignment_qc
+        res = es.search(index="analysis_index", body={"query":{"filtered":{"filter":{"bool":{"must":{"or":[{"terms":{"flags.normal_alignment_qc_report":["false"]}},{"terms":{"flags.tumor_alignment_qc_report":["false"]}}]}}},"query":{"match_all":{}}}}})
+
+        listOfJobs = []
 
         print("Got %d Hits:" % res['hits']['total'])
         for hit in res['hits']['hits']:
             print("%(donor_uuid)s %(center_name)s %(project)s" % hit["_source"])
-            time.sleep(20)
-# LEFT OFF WITH: traverse the structure, pull out info to parameterize bamstats!
-        return AlignmentQCTaskWorker(param=self.es_index_port)
+            for specimen in hit["_source"]["specimen"]:
+                for sample in specimen["samples"]:
+                    for analysis in sample["analysis"]:
+                        if analysis["analysis_type"] == "alignment" and (hit["_source"]["flags"]["normal_alignment_qc_report"] == False and re.match("^Normal - ", specimen["submitter_specimen_type"])) or (hit["_source"]["flags"]["tumor_alignment_qc_report"] == False and re.match("^Primary tumour - |^Recurrent tumour - |^Metastatic tumour -", specimen["submitter_specimen_type"])):
+                            print "HIT!!!! "+analysis["analysis_type"]+" "+str(hit["_source"]["flags"]["normal_alignment_qc_report"])+" "+specimen["submitter_specimen_type"]
+                            listOfJobs.append(AlignmentQCTaskWorker(param=specimen["submitter_specimen_type"]))
+                            # LEFT OFF WITH: traverse the structure, pull out info to parameterize bamstats!
+        return listOfJobs
 
     def run(self):
         f = self.output().open('w')
