62bd19ac02046679609d5c6829c4556a37906a5f
Brian O'Connor <briandoconnor@gmail.com>
2016-08-10 18:25:41 -0700

    updates

--- Dashboard/dashboard_query.py
+++ Dashboard/dashboard_query.py
@@ -137,38 +137,41 @@ def repNum(s):
 
 while True:
     #checking the number of documents
-    res = es.search(index="analysis_index", body={"query": {"match_all": {}}})
-    print("For search for everything, got %d hits:" % res['hits']['total'])
-    for hit in res['hits']['hits']:
-       print("CENTER: %(center_name)s PROGRAM: %(program)s PROJECT: %(project)s DONOR ID: %(submitter_donor_id)s" % hit["_source"])
-    print("\n")
+    try:
+        res = es.search(index="analysis_index", body={"query": {"match_all": {}}})
+        print("For search for everything, got %d hits:" % res['hits']['total'])
+        for hit in res['hits']['hits']:
+           print("CENTER: %(center_name)s PROGRAM: %(program)s PROJECT: %(project)s DONOR ID: %(submitter_donor_id)s" % hit["_source"])
+        print("\n")
 
-    with open("data.json", 'w') as outfile:
-       outfile.write('[')
-       #querying documents using queries above
-       addingcommas = False
-       for q_index in range(len(es_queries)):
-          if (addingcommas):
-             outfile.write(', ')
-          else:
-             addingcommas = True
-          response = es.search(index="analysis_index", body=es_queries[q_index])
-          #print(json.dumps(response, indent=2))
-          count = 0
-          program = "NA"
-          project = "NA"
-          for p in response['aggregations']['project_f']['project'].get('buckets'):
-             count = p.get('doc_count')
-             program = p.get('donor_id').get('buckets')
-             project = p.get('key')
+        with open("data.json", 'w') as outfile:
+           outfile.write('[')
+           #querying documents using queries above
+           addingcommas = False
+           for q_index in range(len(es_queries)):
+              if (addingcommas):
+                 outfile.write(', ')
+              else:
+                 addingcommas = True
+              response = es.search(index="analysis_index", body=es_queries[q_index])
+              #print(json.dumps(response, indent=2))
+              count = 0
+              program = "NA"
+              project = "NA"
+              for p in response['aggregations']['project_f']['project'].get('buckets'):
+                 count = p.get('doc_count')
+                 program = p.get('donor_id').get('buckets')
+                 project = p.get('key')
 
-          print(es_name_query[q_index])
-          print("count: "+str(count))
-          print("program: ", program)
-          print("project: ", project)
-          print("\n")
-          outfile.write('{"Label": "'+es_name_query[q_index]+'", "Count": '+str(count)+'}')
-       outfile.write(']')
+              print(es_name_query[q_index])
+              print("count: "+str(count))
+              print("program: ", program)
+              print("project: ", project)
+              print("\n")
+              outfile.write('{"Label": "'+es_name_query[q_index]+'", "Count": '+str(count)+'}')
+           outfile.write(']')
+    except Exception:
+        print "Some exception, trying again"
 
     # sleep random time before next upload
     print "PAUSING..."
