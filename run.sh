f6dbe1396a43651b4ece3229006af5b3ac0c9ea0
caaespin <caaespin@ucsc.edu>
2017-03-16 02:57:23 +0000

    Moving the fb indexer in here to keep things easier to run

--- run.sh
+++ run.sh
@@ -149,10 +149,76 @@ done
 #Run the metadata-indexer
 python metadata_indexer.py ${ARGUMENTS[@]}
 
+echo "Updating analysis_index"
+curl -XDELETE http://elasticsearch1:9200/analysis_buffer/
+curl -XPUT http://elasticsearch1:9200/analysis_buffer/ -d @analysis_settings.json
+curl -XPUT http://elasticsearch1:9200/analysis_buffer/_mapping/meta?update_all_types  -d @mapping.json
+curl -XPUT http://elasticsearch1:9200/analysis_buffer/_bulk?pretty --data-binary @elasticsearch.jsonl
+
+#####Change buffer to point to alias
+curl -XPOST http://elasticsearch1:9200/_aliases?pretty -d' { "actions" : [ { "remove" : { "index" : "analysis_real", "alias" : "analysis_index" } }, { "add" : { "index" : "analysis_buffer", "alias" : "analysis_index" } } ] }'
+
+##Update real index analysis
+curl -XDELETE http://elasticsearch1:9200/analysis_real/
+curl -XPUT http://elasticsearch1:9200/analysis_real/ -d @analysis_settings.json
+curl -XPUT http://elasticsearch1:9200/analysis_real/_mapping/meta?update_all_types  -d @mapping.json
+curl -XPUT http://elasticsearch1:9200/analysis_real/_bulk?pretty --data-binary @elasticsearch.jsonl
+
+#Change alias one last time from buffer to real
+curl -XPOST http://elasticsearch1:9200/_aliases?pretty -d' { "actions" : [ { "remove" : { "index" : "analysis_buffer", "alias" : "analysis_index" } }, { "add" : { "index" : "analysis_real", "alias" : "analysis_index" } } ] }'
+echo "Starting es_filebrowser_index.py; Creating fb_index"
+#Run the 
+python es_filebrowser_index.py --access public --repoBaseUrl storage.ucsc-cgl.org --repoCountry US --repoName Redwood-AWS-Oregon --repoOrg UCSC --repoType Redwood --repoCountry US
+
+echo "Updating fb_index"
+curl -XDELETE http://elasticsearch1:9200/fb_buffer/
+curl -XPUT http://elasticsearch1:9200/fb_buffer/ -d @fb_settings.json
+curl -XPUT http://elasticsearch1:9200/fb_buffer/_mapping/meta?update_all_types  -d @fb_mapping.json
+curl -XPUT http://elasticsearch1:9200/fb_buffer/_bulk?pretty --data-binary @fb_index.jsonl
+
+###Change alias to point to fb_buffer
+curl -XPOST http://elasticsearch1:9200/_aliases?pretty -d' { "actions" : [ { "remove" : { "index" : "fb_index", "alias" : "fb_alias" } }, { "add" : { "index" : "fb_buffer", "alias" : "fb_alias" } } ] }'
+
+####Index/Update the data in fb_index
+curl -XDELETE http://elasticsearch1:9200/fb_index/
+curl -XPUT http://elasticsearch1:9200/fb_index/ -d @fb_settings.json
+curl -XPUT http://elasticsearch1:9200/fb_index/_mapping/meta?update_all_types  -d @fb_mapping.json
+curl -XPUT http://elasticsearch1:9200/fb_index/_bulk?pretty --data-binary @fb_index.jsonl
+
+#Change alias one last time
+curl -XPOST http://elasticsearch1:9200/_aliases?pretty -d' { "actions" : [ { "remove" : { "index" : "fb_buffer", "alias" : "fb_alias" } }, { "add" : { "index" : "fb_index", "alias" : "fb_alias" } } ] }'
+
+###NOW HANDLE CREATING THE NEW BILLINGINDEX###
+python metadata_indexer.py -preserve-version --skip-program TEST --skip-project TEST  --storage-access-token $storageAccessToken --client-path $clientPath --metadata-schema $metadataSchema --server-host $storageHost
+
+#Populate the billing index
+#Change the Buffer index
+echo "Updating billing_idx"
+curl -XDELETE http://elasticsearch1:9200/billing_buffer/
+curl -XPUT http://elasticsearch1:9200/billing_buffer/
+curl -XPUT http://elasticsearch1:9200/billing_buffer/_mapping/meta?update_all_types  -d @billing_mapping.json
+curl -XPUT http://elasticsearch1:9200/billing_buffer/_bulk?pretty --data-binary @duped_elasticsearch.jsonl
+
+#Change aliases
+curl -XPOST http://elasticsearch1:9200/_aliases?pretty -d' { "actions" : [ { "remove" : { "index" : "billing_real", "alias" : "billing_idx" } }, { "add" : { "index" : "billing_buffer", "alias" : "billing_idx" } } ] }'
+
+#Update real billing_index
+
+curl -XDELETE http://elasticsearch1:9200/billing_real/
+curl -XPUT http://elasticsearch1:9200/billing_real/
+curl -XPUT http://elasticsearch1:9200/billing_real/_mapping/meta?update_all_types  -d @billing_mapping.json
+curl -XPUT http://elasticsearch1:9200/billing_real/_bulk?pretty --data-binary @duped_elasticsearch.jsonl
+
+#Change the alias again, so that billing_idx points again to the real billing_real index
+curl -XPOST http://elasticsearch1:9200/_aliases?pretty -d' { "actions" : [ { "remove" : { "index" : "billing_buffer", "alias" : "billing_idx" } }, { "add" : { "index" : "billing_real", "alias" : "billing_idx" } } ] }'
+
+#TODO: add code to do the daily report generation
+
 #This moves all the .jsonl files to the es-jsonls folder (easier to mount only the jsonl files as opposed to everything else.)
 #find . -name "*.jsonl" -exec cp {} /app/dcc-metadata-indexer/es-jsonls \;
 \cp /app/dcc-metadata-indexer/*jsonl /app/dcc-metadata-indexer/es-jsonls
 
+#curl -XGET http://elasticsearch1:9200/_cat/health
 #Make ownership to that of the executer of the docker image. 
 user=$USER_GROUP
 if [[ ! -z "$user" ]]
