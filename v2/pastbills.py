4c3822e94cb67ef2e9a92b15989a291a99c87141
caaespin <caaespin@ucsc.edu>
2017-04-24 19:38:56 +0000

    Help script for running the billing script since august to april

---
+++ v2/pastbills.py
@@ -0,0 +1,20 @@
+import generate_billings
+
+generate_billings.generate_daily_reports("2016/08/31")
+generate_billings.generate_daily_reports("2016/09/01")
+generate_billings.generate_daily_reports("2016/09/30")
+generate_billings.generate_daily_reports("2016/10/01")
+generate_billings.generate_daily_reports("2016/10/31")
+generate_billings.generate_daily_reports("2016/11/01")
+generate_billings.generate_daily_reports("2016/11/30")
+generate_billings.generate_daily_reports("2016/12/01")
+generate_billings.generate_daily_reports("2016/12/31")
+generate_billings.generate_daily_reports("2017/01/01")
+generate_billings.generate_daily_reports("2017/01/31")
+generate_billings.generate_daily_reports("2017/02/01")
+generate_billings.generate_daily_reports("2017/02/28")
+generate_billings.generate_daily_reports("2017/03/01")
+generate_billings.generate_daily_reports("2017/03/31")
+generate_billings.generate_daily_reports("2016/04/01")
+generate_billings.generate_daily_reports("")
+print "Done"
